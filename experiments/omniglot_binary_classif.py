5afd9c16b15a644c659fc0bb1142ff4983a49ae9
DejasDejas <julien.dejasmin@outlook.fr>
2020-04-22 15:45:04 +0200

    new archi omniglot CNN

--- experiments/omniglot_binary_classif.py
+++ experiments/omniglot_binary_classif.py
@@ -13,24 +13,24 @@ batch_size_test = 64
 train_loader, valid_loader, test_loader = get_omniglot_dataloaders_classification(batch_size_train, batch_size_test)
 
 # parameters default values
-epochs = 100
+epochs = 50
 lr = 1e-3
 momentum = 0.9
 log_interval = 10  # how many batches to wait before logging training status
 criterion = F.nll_loss
 
 # parameters model to load no Binary model
-binary = False
-model_no_binary, name_model = get_my_model_Omniglot(binary)
-print(name_model)
+#binary = False
+#model_no_binary, name_model = get_my_model_Omniglot(binary)
+#print(name_model)
 
-path_model_checkpoint_no_binary = 'trained_models/Omniglot_classif/No_binary_models/'
-path_save_plot_no_binary = 'results/Omniglot_results/plot_acc_loss/Omniglot_classif/'
+#path_model_checkpoint_no_binary = 'trained_models/Omniglot_classif/No_binary_models/'
+#path_save_plot_no_binary = 'results/Omniglot_results/plot_acc_loss/Omniglot_classif/'
 
-print('Begin running No Binary model')
-run(model_no_binary, path_model_checkpoint_no_binary, path_save_plot_no_binary, name_model, train_loader, valid_loader,
-    epochs, lr, momentum, criterion, log_interval)
-print('End running No Binary model')
+#print('Begin running No Binary model')
+#run(model_no_binary, path_model_checkpoint_no_binary, path_save_plot_no_binary, name_model, train_loader, valid_loader,
+#    epochs, lr, momentum, criterion, log_interval)
+#print('End running No Binary model')
 
 # parameters model to load no Binary model
 binary = True
